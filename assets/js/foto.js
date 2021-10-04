833200f8bf9fda841204e26e4f924ff7c4fe5cc1
Alex <alexirschfeld@gmail.com>
2021-10-04 20:22:49 +0200

    modified:   _sass/foto.scss         modified:   admin/config.yml         modified:   assets/js/foto.js

--- assets/js/foto.js
+++ assets/js/foto.js
@@ -1,5 +1,7 @@
 
 const fotoByTag = document.querySelectorAll('#foto-wrapper span');
+
+
 let fotoByName = new Array();
 
 $(document).ready(function () {
@@ -7,6 +9,8 @@ $(document).ready(function () {
 	
 	for (let i = 0; i < fotoByTag.length; i++) {
 		fotoByName.push(fotoByTag[i].innerHTML);
+	};
+
 		$('#foto-wrapper').magnificPopup({
 			delegate: 'a',
 			type: 'image',
@@ -21,7 +25,7 @@ $(document).ready(function () {
 				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
 				titleSrc: function (item) {
 					// return item.el.attr('title') + '<small>Foto: Marsel Van Oosten</small>';
-					return `<small>Foto: ${fotoByName[i]}</small>`;
+					return `<small>Foto: test</small>`;
 				}
 			},
 			callbacks: {
@@ -30,20 +34,7 @@ $(document).ready(function () {
 				}
 			}
 		});
-	};
 
-	const galleryTag = document.querySelectorAll('#foto-wrapper');
-	const galleryTagHeight = $(galleryTag).height();
 
-	$(galleryTag).scroll(function () {
-		let wScroll = $(this).scrollTop();
-		if (wScroll < galleryTagHeight - 1) {
-			$('footer').css('display', 'none')
-		} else {
-			$('footer').css('display', 'flex')
-		}
-		// console.log(wScroll);
-	});
-	// jQuery ends here
 });
 
