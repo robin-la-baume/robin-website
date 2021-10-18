b8005f610a0a187c84b2b2f1a306c6036b9804c7
Alex <alexirschfeld@gmail.com>
2021-10-18 20:44:18 +0200

    modified:   _includes/footer.html         modified:   _includes/head.html         new file:   assets/css/cookie.css         deleted:    assets/css/cookieconsent.min.css         modified:   assets/css/main.scss         modified:   assets/js/cookie.js         deleted:    assets/js/cookieconsent.min.js

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,19 +1,41 @@
-window.addEventListener("load", function(){
-  window.cookieconsent.initialise({
-      "palette": {
-          "popup": {
-              "background": "rgba(0, 0, 0, 0)",
-              "text": "#fffff"
-          },
-          "button": {
-              "background": "rgba(0, 0, 0, 0)"
-          }
-      },
-      "theme": "edgeless",
-      "content": {
-          "message": "🍪 Diese Seite verwendet Cookies.",
-          "dismiss": "OK",
-          "link": "",
-          "href": "imprint.html"
-      }
-  })});
+function getCookie(cname) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+
+    const expires = "expires="+ d.toUTCString();
+
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+const cookieBanner = document.querySelector('#cookie-banner');
+const hasCookieConsent = getCookie('cookies-consent');
+
+if (!hasCookieConsent) {
+    cookieBanner.classList.remove('hidden');
+}
+
+const consentCta = cookieBanner.querySelector('#consent-cookies');
+
+consentCta.addEventListener('click', () => {
+    cookieBanner.classList.add('hidden');
+    setCookie('cookies-consent', 1, 365);
+});
